f0f14dd745b3172c4bbebd96cc2afa354a54f248
Shrivatsa KS <54031265+Shrivatsa2904@users.noreply.github.com>
2020-11-05 11:00:15 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,8 @@
 
 module.exports = {
     google : {
-        clientID: "323442307574-njnoo751l8kct44kfb1ikgtsse2jep70.apps.googleusercontent.com",
-        clientSecret: "t-7REzZh_l5MW9SPBKc5Jn2b"
+        clientID: "clientID",
+        clientSecret: "clientsecret"
     },
     session: {
         cookieKey: 'shrivatsaksquickworkassignment'
